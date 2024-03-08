1cf5a6d2c94548252be0562abf158eb090b7640b
ewatson95 <ewan.watson@shell.com>
2024-03-08 21:09:05 +0000

    spin up server

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
 )
 
+func handleHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World!"))
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /", handleHome)
+
+	log.Print("Starting on port :3000")
+	err := http.ListenAndServe("localhost:3000", mux)
+	log.Fatal(err)
 }
