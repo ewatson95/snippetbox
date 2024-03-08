80470d57049e1a1d85547053813a553b8b0f9160
ewatson95 <ewan.watson@shell.com>
2024-03-08 21:37:16 +0000

    Add handlers for view and create

--- main.go
+++ main.go
@@ -5,16 +5,28 @@ import (
 	"net/http"
 )
 
+const port = ":3000"
+
 func handleHome(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World!"))
+	w.Write([]byte("Home Page"))
+}
+
+func handleSnippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a snippet"))
+}
+
+func handleSnippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a snippet"))
 }
 
 func main() {
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /", handleHome)
+	mux.HandleFunc("GET /snippet/view", handleSnippetView)
+	mux.HandleFunc("POST /snippet/create", handleSnippetCreate)
 
-	log.Print("Starting on port :3000")
-	err := http.ListenAndServe("localhost:3000", mux)
+	log.Printf("Starting on port %s", port)
+	err := http.ListenAndServe(port, mux)
 	log.Fatal(err)
 }
