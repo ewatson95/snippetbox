3c031c9e3d20a1a49061d3b91d725f9d899f240b
ewatson95 <ewan.watson@shell.com>
2024-03-10 09:03:15 +0000

    add templating structure for pages

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,12 +2,27 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
 
 func handleHome(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Home Page"))
+	files := []string{"./ui/html/base.tmpl", "./ui/html/pages/home.tmpl"}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
 }
 
 func handleSnippetView(w http.ResponseWriter, r *http.Request) {
